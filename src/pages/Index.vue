dbefd97e9e3fa97b09fcd43d507183ff97fe252f
TheZuzanka <wlachovska.zuzana@gmail.com>
2020-12-05 15:55:44 +0100

    initial commit II

---
+++ src/pages/Index.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page class="flex flex-center">
+    {{ message }}
+  </q-page>
+</template>
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      message: ''
+    }
+  },
+  mounted () {
+    this.$axios
+      .get('http://localhost:8888/api/admin')
+      .then(res => {
+        console.log('res', res)
+        this.message = res.data
+      })
+      .catch(err => {
+        console.log('err', err)
+      })
+  }
+}
+</script>
